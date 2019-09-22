873d69c6e06f094593e73f4ea11f1d8d473c1a00
Luis Alejandro Canchola <lacp532.la@gmail.com>
2019-09-22 02:15:39 -0500

    Update Jugar.py

--- Jugar.py
+++ Jugar.py
@@ -1,30 +1,44 @@
 from juegodelavida import juegodelavida
-
+import time
+import os
 def main():
-    fil=5
-    col=5
     
     print("--------------------JUEGO DE LA VIDA--------------------------")
-    inicio= [[1,3],[2,2],[2,3],[2,4]]
-    for i in range (5):
-        partida= juegodelavida(fil,col,i,inicio)
+    cuad=int(input("-Introduzca el valor del cuadro a generar-"))
+    gen=int(input("-cuantas generaciones quieres?-"))+1
+    live=int(input("-Celulas vivas?-"))
+    inicio=[]
+   
+    for i in range(live):
+        tmp=i+1
+        Fil = int(input(f"Fila para celula ({tmp}):"))
+        Col = int(input(f"Columna para celula ({tmp}):"))
+        inicio.append([(Fil-1), (Col-1)])
+        os.system ("cls") 
+    partida= juegodelavida(cuad,cuad,gen,inicio)
+    print("Iniciando Juego.....")
+    time.sleep(2)
+    print("       Generacion 1      ")
+    partida.to_string()
+    print("Celulas vivas:", len(inicio))
+    print("--------------------------------------------------------")
+    for i in range(2,gen):
+        nueva = []
+        print(f"       Generacion {i}       ")
+        for fila in range(cuad):
+            for columna in range(cuad):
+                vivos = partida.get_num_live_neighbors(fila, columna)
+                if partida.is_live_cell(fila, columna):
+                    if vivos == 3 or vivos == 2:
+                        nueva.append([fila, columna])
+                else:
+                    if vivos == 3:
+                        nueva.append([fila, columna])         
+        partida.configure_next_generation(nueva)
         partida.to_string()
-    
-        for i in range(2,5):
-            nueva = []
-            for fila in range(fil):
-                for columna in range(col):
-                    vivos = partida.get_num_live_neighbors(fila, columna)
-                    if partida.is_live_cell(fila, columna):
-                        if vivos == 3 or vivos == 2:
-                            nueva.append([fila, columna])
-                    else:
-                        if vivos == 3:
-                            nueva.append([fila, columna])         
-            partida.configure_next_generation(nueva)
-            partida.to_string()
-        print("Las celulas vivas son",vivos)
-        print("_______________________________Generación",i+1," ____________________________")
+        print("Celulas Vivas:", len(nueva))
+        print("--------------------------------------------------------")
+    print("_______________________________Generación",i+1," ____________________________")
 
             
 main()
